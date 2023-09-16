fe98392f8d396d780636fae2bc3e6345baf656f4
pablo <paulmamaniventura@gmail.com>
2023-09-16 08:58:31 -0500

    agregado de la seccion about(html, css)

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// const texto = "Hola! Soy Clever Paul";
+// const textoEscrito = document.querySelector('.texto-maquina');
+// let indice = 0;
+
+
+
+// function maquinaEscribir(){
+//     // while(indice < texto.length){
+
+//     // }
+//     if(indice < texto.length){
+//         textoEscrito.textContent += texto.charAt(indice);
+//         indice++;
+        
+//         return textoEscrito;
+//     }
+   
+// }
+
+// maquinaEscribir();
+// setTimeout(maquinaEscribir, 200);
